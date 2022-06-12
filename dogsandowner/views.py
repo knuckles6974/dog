4a7be203b39ff9030fb84acf2dd277c3001a1c97
knuckles6974 <knuckles6974@gmail.com>
2022-06-12 20:58:30 +0900

    changeview

--- dogsandowner/views.py
+++ dogsandowner/views.py
@@ -8,10 +8,13 @@ from django.http      import JsonResponse
 # Create your views here.
 class DogsandownerView(View):
     def post(self, request):
-          data = json.loads(request.body)
+        data = json.loads(request.body)
 
-          dog = Owner.objects.create(dog=data["dog_name"])
-          type = Owner.objects.create(type=data["type"])
-          owner = Owner.objects.create(name=data["owner_name"])
-
-          return JsonResponse({"message" : "댕댕이 등록완료"}, status = 201)
+        Owner.objects.create(
+            dog=data["dog_name"],
+            type=data["type"],
+            name=data["owner_name"],
+                                
+        )
+        
+        return JsonResponse({"message" : "댕댕이 등록완료"}, status = 201)
